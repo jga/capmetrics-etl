0b6b90a91390551fffebacea55e9cccb4fa3d277
Julio Gonzalez Altamirano <devjga@gmail.com>
2016-04-02 22:34:07 -0500

    Add configuration parsing to CLI.

--- capmetrics_etl/cli.py
+++ capmetrics_etl/cli.py
@@ -1,14 +1,36 @@
 import click
+import configparser
+
+import json
 from . import etl
 
 
+
+
+def parse_capmetrics_configuration(config_parser):
+    worksheet_names = json.loads(config_parser['capmetrics']['worksheet_names'])
+    capmetrics_configuration = {
+        'timezone': 'America/Chicago',
+        'engine': config_parser['capmetrics']['engine'],
+        'worksheet_names': worksheet_names
+    }
+    if 'timezone' in config_parser['capmetrics']:
+        capmetrics_configuration['timezone'] = config_parser['capmetrics']['timezone']
+    return capmetrics_configuration
+
 @click.command()
+@click.argument('config')
 @click.option('--test', default=False)
-def run(test):
+def run(config, test):
     if not test:
-        etl.run_excel_etl()
+        config_parser = configparser.ConfigParser()
+        # make parsing of config file names case-sensitive
+        config_parser.optionxform = str
+        config_parser.read(config)
+        capmetrics_configuration = parse_capmetrics_configuration(config_parser)
+        etl.run_excel_etl(capmetrics_configuration)
     else:
-        click.echo('Capmetrics test.')
+        click.echo('Capmetrics CLI test.')
 
 # Call run function when module deployed as script. This is approach is common
 # within the python community

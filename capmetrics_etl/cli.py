005abb868f3767b2007e85960e9ca0b227648565
Julio Gonzalez Altamirano <devjga@gmail.com>
2016-05-17 14:06:46 -0500

    Remove source and output from configuration file parsing.

--- capmetrics_etl/cli.py
+++ capmetrics_etl/cli.py
@@ -8,13 +8,9 @@ from capmetrics_etl.quality import check_quality
 
 
 def parse_capmetrics_configuration(config_parser):
-    source = config_parser['capmetrics']['source']
-    output = config_parser['capmetrics']['output']
     daily_worksheets = json.loads(config_parser['capmetrics']['daily_ridership_worksheets'])
     hourly_worksheets = json.loads(config_parser['capmetrics']['hour_productivity_worksheets'])
     capmetrics_configuration = {
-        'source': source,
-        'output': output,
         'engine_url': config_parser['capmetrics']['engine_url'],
         'daily_ridership_worksheets': daily_worksheets,
         'hour_productivity_worksheets': hourly_worksheets
@@ -23,10 +19,12 @@ def parse_capmetrics_configuration(config_parser):
 
 
 @click.command()
+@click.argument('file')
 @click.argument('config')
 @click.option('--test', is_flag=True)
-def etl(config, test):
+def etl(file, config, test):
     if not test:
+        click.echo('Capmetrics Excel ETL starting...')
         config_parser = configparser.ConfigParser()
         # make parsing of config file names case-sensitive
         config_parser.optionxform = str
@@ -36,7 +34,7 @@ def etl(config, test):
         daily_worksheets = capmetrics_configuration['daily_ridership_worksheets']
         hour_worksheets = capmetrics_configuration['hour_productivity_worksheets']
         worksheet_names = daily_worksheets + hour_worksheets
-        if check_quality(capmetrics_configuration['source'], worksheet_names):
+        if check_quality(file, worksheet_names):
             engine = create_engine(capmetrics_configuration['engine_url'])
             has_table = engine.dialect.has_table(engine.connect(), 'route')
             if not has_table:
@@ -44,7 +42,7 @@ def etl(config, test):
             Session = sessionmaker()
             Session.configure(bind=engine)
             session = Session()
-            run_excel_etl(capmetrics_configuration, session)
+            run_excel_etl(file, session, capmetrics_configuration)
             click.echo('Capmetrics Excel ETL completed.')
         else:
             click.echo('Capmetrics stopped ETL. Source file data is incorrectly formatted.')
